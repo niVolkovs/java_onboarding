d6a2bd0eb9d5976bc4931f11abf3ef85489546e3
niVolkovs <146339296+niVolkovs@users.noreply.github.com>
2023-09-29 08:28:02 +0300

    Create assert SquareTests

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,25 +4,16 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("Volkov");
 
-        double l = 5;
-        System.out.println("Площадь квадрата = "+area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника = "+area(a,b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
     }
 
     public static void hello(String s) {
         System.out.println("Hello " + s + " Wo");
-    }
-
-    public static double area (double len){
-        return len*len;
-    }
-
-    public static double area (double a, double b){
-        return a*b;
     }
 
 }
