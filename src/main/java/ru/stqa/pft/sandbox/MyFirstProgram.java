e85c2b0de63e78d8f06f13bc343366ec9e098d49
niVolkovs <146339296+niVolkovs@users.noreply.github.com>
2023-09-29 07:25:20 +0300

    Добавление функций

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
-		System.out.println("Hello, world");
-}
+    public static void main(String[] args) {
+        hello("Volkov");
+
+        double l = 5;
+        System.out.println("Площадь квадрата = "+area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника = "+area(a,b));
+
+    }
+
+    public static void hello(String s) {
+        System.out.println("Hello " + s + " Wo");
+    }
+
+    public static double area (double len){
+        return len*len;
+    }
+
+    public static double area (double a, double b){
+        return a*b;
+    }
+
 }
